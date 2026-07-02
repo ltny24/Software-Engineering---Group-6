d54f7125de21f80011453276f85edf62605acf71
htnngoc2418-art <htnngoc2418@clc.fitus.edu.vn>
2026-07-02 22:31:30 +0700

    Delete src/backend/src/test/java/com/myus/repository directory

--- src/backend/src/test/java/com/myus/repository/AcademicRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.myus.repository;
-
-import com.myus.entity.AcademicRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AcademicRecordRepository extends JpaRepository<AcademicRecord, Long> {
-}
